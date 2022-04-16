ab4fa2b04d648a175d62d9e2a9ac4a8eed91f0e7
Kevin Li <kevinsl192k@gmail.com>
2022-04-16 12:13:46 -0700

    tailwind implemented

--- client/src/App.js
+++ client/src/App.js
@@ -3,34 +3,13 @@ import './App.css';
 import React, { Component } from 'react';
 import { GoogleMap, LoadScript } from '@react-google-maps/api';
 
-
-const containerStyle = {
-  width: '400px',
-  height: '400px'
-};
-
-const center = {
-  lat: -34,
-  lng: 151
-};
-
-class MyComponents extends Component {
-  render() {
-    return (
-      <LoadScript
-        googleMapsApiKey="AIzaSyCfB1IHaGFizVCHLWOZJsm3-UwabmKV-eo"
-      >
-        <GoogleMap
-          mapContainerStyle={containerStyle}
-          center={center}
-          zoom={10}
-        >
-          { /* Child components, such as markers, info windows, etc. */ }
-          <></>
-        </GoogleMap>
-      </LoadScript>
-    )
-  }
+function App() {
+  return (
+    <div className=''>
+      testing
+    </div>
+  );
 }
 
-export default MyComponents;
+export default App;
+
