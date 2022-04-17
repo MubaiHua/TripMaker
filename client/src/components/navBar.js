97172ac5fad4b6b5d18d674e89e65e242dc016f7
Kevin Li <kevinsl192k@gmail.com>
2022-04-16 18:28:37 -0700

    some commit

--- client/src/components/navBar.js
+++ client/src/components/navBar.js
@@ -2,18 +2,17 @@ import { waitFor } from "@testing-library/react";
 import React from "react";
 
 function NavBar(props) {
-
-    function displayDropdown() {
-        document.getElementById("Dropdown").classList.remove('hidden');
-        document.getElementById("Dropdown").classList.add('translate-y-2');
-    }
+    
+    function playAudio(url) {
+        new Audio(url).play();
+      }
 
     return (
         <div className="w-full h-69px justify-end flex flex-row items-center bg-candy-pink/25 border-b-2 border-pastel-brown">
             <p className="text-14px font-roboto-slab mr-600px w-1/12">
-                ROOM CODE: <p className="text-gray-500">code</p>
+                ROOM CODE: <span className="text-gray-500">code</span>
             </p>
-            <button className="text-14px font-roboto-slab transition-all duration-400 ease-in hover:bg-candy-pink/40 hover:h-full h-4/6 w-1/12">
+            <button onClick={playAudio("https://static.wikia.nocookie.net/valorant/images/1/18/ReynaLeerCast1.mp3/revision/latest?cb=20210617145121")} className="text-14px font-roboto-slab transition-all duration-400 ease-in hover:bg-candy-pink/40 hover:h-full h-4/6 w-1/12">
                 Dark Mode
             </button>
             <button className="text-14px font-roboto-slab transition-all duration-400 ease-in hover:bg-candy-pink/40 hover:h-full h-4/6 w-1/12">
@@ -28,3 +27,4 @@ function NavBar(props) {
 }
 
 export default NavBar;
+
