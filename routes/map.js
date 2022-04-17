55bade92e0e968ea5810db27a7f0c2add27b063f
Mubai Hua <hmbyxfyj@gmail.com>
2022-04-17 00:39:39 -0700

    backend for authentication

--- routes/map.js
+++ routes/map.js
@@ -5,6 +5,7 @@ const router = express.Router();
 router.route('/createMap').post(mapController.createMap);
 router.route('/updateMap').post(mapController.updateMap);
 router.route('/getMap').post(mapController.getMap);
+router.route('/findCode').post(mapController.findCode);
 
 module.exports = router;
 
